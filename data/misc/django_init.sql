af3a676822b2deb425192c6a8c7ea53d0e55fca5
mrdover99 <91235168+mrdover99@users.noreply.github.com>
2021-12-10 18:59:44 -0800

    Add files via upload

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS rocket_league_db;
+CREATE DATABASE rocket_league_db;
+
+\c rocket_league_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
